b164423c71eb107cbde6fd2b3e31a6d457b10baa
nawazshareefshaiksfdc <147690940+nawazshareefshaiksfdc@users.noreply.github.com>
2024-03-05 11:34:39 +0530

    i created a hmtl page

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let name = 'nawaz shareef';
 let age = 26;
 let details = {name:"nawaz shareef",age:26}
 console.log(fruits[0]);
-
+console.log(details)
 
 // to run this code type index.js in terminal
